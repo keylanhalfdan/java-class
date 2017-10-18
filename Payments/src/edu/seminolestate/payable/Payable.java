8110e1d6acafd17161d88d763577c06b1983cfdc
keylanhalfdan <xerobourne@Gmail.com>
2017-10-17 21:37:38 -0400

    Delete Payable.java

--- Payments/src/edu/seminolestate/payable/Payable.java
+++
@@ -1,9 +0,0 @@
-package edu.seminolestate.payable;
-/*
-* AUTHOR:	R Kaye
-* DATE:	6/30/2017
-* This is an interface to handle define items as Payable.
-*/
-public interface Payable {
-	public abstract double computeAmountToPay();
-}
